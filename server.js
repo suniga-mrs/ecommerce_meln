3a8dde3ab316aa4c378a099a206ab83ea428f6b5
Michelle Suniga <suniga.mrs@gmail.com>
2019-02-12 15:59:25 +0800

    Hosting checkpoint

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb+srv://mich_admin:admin00000000@mongo-project-db-6eyre.mongodb.net/test?retryWrites=true', {useNewUrlParser: true});
+
+const databaseUrl = process.env.DATABASE_URL || 'mongodb+srv://mich_admin:admin00000000@mongo-project-db-6eyre.mongodb.net/test?retryWrites=true';
+
+mongoose.connect(databaseUrl, {useNewUrlParser: true});
+mongoose.connection.once('open', ()=> {
+	console.log('Remote Database Connection Established');
+})
 
 const app = express();
 
@@ -15,6 +21,8 @@ app.use( (err, req, res, next) => {
 	res.status(422).send({error : err.message})
 })
 
-app.listen(5000, () => {
-	console.log(`Example app listening on port 5000!`);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`);
 });
